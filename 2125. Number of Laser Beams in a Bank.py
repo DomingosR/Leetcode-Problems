689a1d0a5875119c983aa31f5c946ab069234d9f
Domingos Romualdo <DAFR1729@outlook.com>
2024-05-21 20:31:29 -0400

    Delete 2125. Number of Laser Beams in a Bank.py

--- 2125. Number of Laser Beams in a Bank.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def numberOfBeams(self, bank):
-        n = len(bank)
-        devicesPerRow = [bank[i].count("1") for i in range(n)]
-        auxList = [devicesPerRow[i] for i in range(n) if devicesPerRow[i] > 0]
-        return sum([d1 * d2 for d1, d2 in list(zip(auxList, auxList[1:]))])
