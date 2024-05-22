ea5326f8e04e442dda034e38d8b3f623cd8d8864
Domingos Romualdo <DAFR1729@outlook.com>
2024-05-21 20:35:28 -0400

    Delete 1934. Confirmation Rate.sql

--- 1934. Confirmation Rate.sql
+++
@@ -1,5 +0,0 @@
-SELECT     Signups.user_id, ROUND(AVG(IF(Confirmations.action = "confirmed", 1, 0)), 2) AS confirmation_rate
-FROM       Signups
-LEFT JOIN  Confirmations
-ON         Signups.user_id = Confirmations.user_id
-GROUP BY   user_id;
